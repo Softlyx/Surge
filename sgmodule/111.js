14528134ff151e715358e813c4ca356c75d7a610
Softlyx <109867095+Softlyx@users.noreply.github.com>
2023-12-02 14:21:49 +0800

    Delete sgmodule/111.js

--- sgmodule/111.js
+++
@@ -1,9 +0,0 @@
-^https:\/\/richmanrules\.ksedt\.com\/intellectWaterfall(Bidding)?\/find url reject
-
-^https?:\/\/richmanapi\.jxedt\.com\/api\/(ad|adplus|banadplus)\/ url reject-200
-
-^https?:\/\/api\.jxedt\.com\/ad\/ url reject
-
-^https?:\/\/api\.jxedt\.com\/jump\/EMiCcDNp url reject-img
-
-^https?:\/\/richmanmain\.jxedt\.com\/advertisement\/fallback url reject-img

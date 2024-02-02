c6ff8614703f77d276445572a0f5ca081a177fb1
Softlyx <109867095+Softlyx@users.noreply.github.com>
2024-02-02 10:24:02 +0800

    Delete sgmodule/test.js

--- sgmodule/test.js
+++
@@ -1,9 +0,0 @@
-var modifiedHeaders = $request.headers;
-var operationName = modifiedHeaders['x-apollo-operation-name'];
-
-if (operationName == "GetCurrentUser"||operationName == "GetUserById") {
-  var body = $response.body.replace(/"premium":false/g, '"premium":true');
-  $done({ body: body });
-} else {
-  $done({});
-}
